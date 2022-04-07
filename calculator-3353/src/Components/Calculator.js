78e4f86c391c0252cdc088718d9c9f350450aec5
Jarrod Hasnain <jhasnain@uwo.ca>
2022-04-06 21:58:03 -0400

    Created App Skeleton

---
+++ calculator-3353/src/Components/Calculator.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Box, Typography } from '@mui/material';
+
+function Calculator() {
+
+    return(
+        <Box>
+            <Typography>
+                Hello!
+            </Typography>
+        </Box>
+    )
+}
+
+export default Calculator;
